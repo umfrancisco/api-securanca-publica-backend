b93c6aa9430167940c7672fd60b418ff8dfc540d
umfrancisco <francis.guitler@gmail.com>
2026-07-04 16:46:05 -0300

    First commit

---
+++ src/main/java/com/umfrancisco/app/ApiSegurancaPublicaApplication.java
@@ -0,0 +1,13 @@
+package com.umfrancisco.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSegurancaPublicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSegurancaPublicaApplication.class, args);
+	}
+
+}
